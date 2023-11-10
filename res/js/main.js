f509c1e64a333905cea23ef06c5d51024414538a
BlackFox <rylich.honza@seznam.cz>
2023-11-10 08:16:03 +0100

    js start dis

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,4 @@
-/*
+ /*
         parametrz
 function tomas(item, item2){
 console.log("nesu "+ item + "," + item2);    
@@ -47,4 +47,18 @@ let rresultA = sum(5, 4);
 let rresultB = sum(7, 4);
 let rresult = sum(rresultA, rresultB);
 console.log(rresult)*/
+
+const point = document.getElementById("point");
+const start = document.getElementById("start");
+
+start.onclick = () => {
+  start.style.display = "none";
+  moveElement(point, 300,200)
+
+}
+
+const moveElement = (elemnet, x, y) =>{
+elemnet.style.top = y + "px";
+elemnet.style.left = x + "px"
+}
 
